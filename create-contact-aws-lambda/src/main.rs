bbe9f4394e24e2aadd75dc69b3081adf37360240
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2022-03-08 03:25:58 -0300

    Add .gitignore

--- create-contact-aws-lambda/src/main.rs
+++ create-contact-aws-lambda/src/main.rs
@@ -1,33 +1,69 @@
-use lambda_runtime::{Context, handler_fn, error::HandlerError};
-use log::LevelFilter;
-use serde::{Deserialize, Serialize};
-use simple_logger::SimpleLogger;
-use std::error::Error;
+use lambda_http::{http::StatusCode, service_fn, Error, IntoResponse, Request, Response};
+use serde::{Deserialize};
+use serde_json::json;
+use tracing::{info, warn};
 
-#[derive(Deserialize)]
-struct Request {
+#[derive(Deserialize, Debug)]
+struct RequestBody {
     first_name: String,
     last_name: String,
 }
 
-#[derive(Serialize)]
-struct Response {
-    msg: String,
-}
-
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
-
-    let func = handler_fn(handler);
-    lambda_runtime::run(func).await?;
+async fn main() -> Result<(), Error> {
+    lambda_http::run(service_fn(handler)).await?;
     Ok(())
 }
 
-async fn handler(event: Request, _: Context) -> Result<Response, HandlerError> {
-    let resp = Response {
-        msg: format!("Hello {}!", event.first_name),
-    };
+async fn handler(event: Request) -> Result<impl IntoResponse, Error> {
+    let body: RequestBody = match event.payload() {
+        Ok(Some(body)) => body,
+        Ok(None) => {
+            warn!("Missing request body")
+            return Ok(response(
+                StatusCode::BAD_REQUEST,
+                json!({ "message": "Missing request body"}).to_string()
+            ));
+        }
+        Err(err) => {
+            warn!("Failed to parse request body: {}", err);
+            return Ok(response(
+                StatusCode::BAD_REQUEST,
+                json!({"message": "Failed to parse body"}).to_string()
+            ));
+        }
+    }
+    info!("Body: {:?}", body);
 
-    Ok(resp)
+    Ok(response(
+        StatusCode::OK,
+        json!({"message": "Hello World!"}).to_string()
+    ))
+}
+
+fn response(status_code: StatusCode, body: String) -> Response<String> {
+    Response::builder()
+        .status(status_code)
+        .header("Content-Type", "application/json")
+        .body(body)
+        .unwrap()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_handler() {
+        let request = lambda_http::http:Request::builder()
+            .body(json!({"first_name": "John", "last_name": "Doe"}).to_string());
+        let expected = json!({
+            "message": "Hello World!"
+        }).into_response();
+        let response = handler(request)
+            .await
+            .expect("expected Ok(...) value")
+            .into_response();
+        assert_eq!(response.body(), expected.body())
+    }
 }
